f04061db9157cd6b9f5e08f4f44ac5bbf1c8bab0
ZhangNX <504947517@qq.com>
2016-11-19 15:13:14 +0800

    11.18练习

---
+++ Servlet/src/cn/edu/bjfu/en/im/homework_6/logout.java
@@ -0,0 +1,39 @@
+package cn.edu.bjfu.en.im.homework_6;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class logout
+ */
+@WebServlet("/logout.do")
+public class logout extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+    public logout() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		session.invalidate();
+		RequestDispatcher rd = request.getRequestDispatcher("login.html");
+		rd.forward(request, response);
+		
+	}
+
+}
